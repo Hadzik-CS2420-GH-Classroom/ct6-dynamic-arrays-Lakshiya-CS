008141c8ee19e829239065a0d09ce5efcfcd9257
Lakshiya-CS <lsenavaraiyanlavanya@mail.weber.edu>
2026-02-25 12:14:16 -0700

    done with ct6.

--- src/two_dimensional_arrays.cpp
+++ src/two_dimensional_arrays.cpp
@@ -168,7 +168,7 @@ void twoDimensionalArrays() {
 
     // TODO: Free the flat array with delete[] and set to nullptr
 
-    std::cout<<"Flate array freed(just one delete[]!)"<<'\n';
+    std::cout<<"Flat array freed(just one delete[]!)"<<'\n';
     delete[] flat;
     flat=nullptr;
 
